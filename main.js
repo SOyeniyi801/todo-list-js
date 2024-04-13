3171cf425fe199f8b0573316a8b46bccb3d9f4f1
soyeniyi801 <naturalbeauty801@gmail.com>
2024-04-13 09:06:21 -0400

    Logic: new task adds to list

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-document.getElementById('btnAddTask').addEventListener('click', () => {
-    const newTaskValue = document.getElementById('taskInput').value 
-    console.log(newTaskValue)})
+document.getElementById("btnAddTask").addEventListener("click", () => {
+  const newTaskValue = document.getElementById("taskInput").value;
+  const newTaskItem = document.createElement("li");
+  newTaskItem.textContent = newTaskValue;
 
+  const newListItem = document.getElementById("taskList");
+  newListItem.append(newTaskItem);
+});
