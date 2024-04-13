da3c8043d76a5980e261f2e1120ff2b81b0f6016
soyeniyi801 <naturalbeauty801@gmail.com>
2024-04-13 09:28:29 -0400

    Logic: add checkbox

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 document.getElementById("btnAddTask").addEventListener("click", () => {
   const newTaskValue = document.getElementById("taskInput").value;
   const newTaskItem = document.createElement("li");
-  newTaskItem.textContent = newTaskValue;
+
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  newTaskItem.appendChild(checkbox);
+
+  const label = document.createElement("label");
+  label.textContent = newTaskValue;
+  newTaskItem.appendChild(label);
 
   const newListItem = document.getElementById("taskList");
-  newListItem.append(newTaskItem);
+  newListItem.appendChild(newTaskItem);
+
+  document.getElementById("taskInput").value = "";
 });
